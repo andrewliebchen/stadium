19ed2935fafd838a36a5f0367d0df83bc505b2ac
andrewliebchen <andrewliebchen@gmail.com>
2014-05-19 23:14:37 -0700

    Additional basic styles

--- server/server.js
+++ server/server.js
@@ -50,3 +50,17 @@ Meteor.methods({
     Messages.update(messageId, {$set: {message: value}});
   }
 });
+
+// User accounts
+Accounts.onCreateUser(function(options, user) {
+  if (options.profile) {
+    user.profile = options.profile;
+  }
+
+  user.profile.github = {};
+  user.profile.github.accessToken = user.services.github.accessToken;
+  user.profile.github.email = user.services.github.email;
+  user.profile.github.username = user.services.github.username;
+
+  return user;
+});
