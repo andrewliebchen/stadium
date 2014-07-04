1ec8cc63cbc26d3028b6322b610a9b40fd56524e
andrewliebchen <andrewliebchen@gmail.com>
2014-07-04 15:27:33 -0700

    More chat styling

--- client/index.js
+++ client/index.js
@@ -2,9 +2,13 @@ Session.set('currentChat', null);
 
 Template.launchbar.events({
   'click #chat_all' : function(event) {
-    $(event.target).toggleClass('is-selected');
     Session.set('currentChat', null);
+
+    $(event.target).toggleClass('is-selected');
     $('body').toggleClass('show-chats');
     isotopeLayout();
+
+    var chatsWidth = $('.chats').width();
+    $('.new-chat').css('width', chatsWidth + 'px');
   }
 });
