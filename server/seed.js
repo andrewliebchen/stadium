004565a5ecbe986a57c009e42c6d4b9e3a956d91
andrewliebchen <andrewliebchen@gmail.com>
2014-07-06 19:08:28 -0700

    Ticket size tweaks

--- server/seed.js
+++ server/seed.js
@@ -1,10 +1,12 @@
 Meteor.startup(function() {
   // Do this when the server starts
 
+  // Tickets.remove({});
+
   if(Tickets.find().count() === 0) {
     Tickets.insert({
       userId: Meteor.userId,
-      title:  'Install Quickbooks',
+      title:  'quickbooks',
       description: 'As an accountant I want Quickbooks installed so that I can update the books',
       type:   'story',
       size:   'small',
@@ -13,7 +15,7 @@ Meteor.startup(function() {
     });
     Tickets.insert({
       userId: Meteor.userId,
-      title:  'Add robots',
+      title:  'robots',
       description: 'As a developer I want a robot so that the robot can do my work',
       type:   'story',
       size:   'small',
@@ -22,7 +24,7 @@ Meteor.startup(function() {
     });
     Tickets.insert({
       userId: Meteor.userId,
-      title:  'Productivity reports',
+      title:  'reports',
       description: "As a manager I want productivity reports so that I don't have to pester my team for updates",
       type:   'story',
       size:   'medium',
@@ -31,7 +33,7 @@ Meteor.startup(function() {
     });
     Tickets.insert({
       userId: Meteor.userId,
-      title:  'Homepage Typo',
+      title:  'typo',
       description: 'The homepage should say "Get your freak on," instead of "Get your frank on."',
       type:   'defect',
       size:   'small',
@@ -40,16 +42,7 @@ Meteor.startup(function() {
     });
     Tickets.insert({
       userId: Meteor.userId,
-      title:  'Homepage Typo',
-      description: 'The homepage should say "Get your freak on," instead of "Get your frank on."',
-      type:   'defect',
-      size:   'small',
-      status: 'current',
-      time:   Date.now()
-    });
-    Tickets.insert({
-      userId: Meteor.userId,
-      title:  'Change the go button to red',
+      title:  'gobutton',
       type:   'task',
       size:   'small',
       status: 'current',
