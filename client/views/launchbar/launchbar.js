cf5d1b4a844cbe7ab2d1b9524b18bbdfb8e21299
andrewliebchen <andrewliebchen@gmail.com>
2014-07-12 19:16:40 -0700

    Clean up commandline alignment

--- client/views/launchbar/launchbar.js
+++ client/views/launchbar/launchbar.js
@@ -8,12 +8,13 @@ Template.launchbar.members = function() {
   return Meteor.users.find({_id: {$ne: Meteor.userId()}});
 };
 
-Template.userMenu.currentUserAvatar = function() {
-  var user = Meteor.user();
-  if (user && user.emails[0].address) {
-    return '<img src="' + Gravatar.imageUrl(user.emails[0].address) + '">';
-  }
-};
+// Make this work
+// Template.userMenu.currentUserAvatar = function() {
+//   var user = Meteor.user();
+//   if (user && user.emails[0].address) {
+//     return '<img src="' + Gravatar.imageUrl(user.emails[0].address) + '">';
+//   }
+// };
 
 Template.launchbar.events({
   'click .chat-toggle' : function(event) {
@@ -21,7 +22,6 @@ Template.launchbar.events({
     Session.set('currentChatParent', this._id);
 
     if($this.hasClass('is-selected')) {
-      $('.chat-toggle.is-selected').removeClass('is-selected');
       $('body').removeClass('show-messages');
       $this.removeClass('is-selected');
     } else {
