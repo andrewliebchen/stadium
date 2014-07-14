97fbe849e8936c2b6399958ee0b198e730aa5740
andrewliebchen <andrewliebchen@gmail.com>
2014-07-13 21:38:55 -0700

    Wait for /add command on commandline

--- client/views/commandline/commandline.js
+++ client/views/commandline/commandline.js
@@ -1,22 +1,26 @@
 Template.commandline.events({
   'keydown .commandline-input' : function(event) {
     if (event.which == 13) {
-      Meteor.user() ? name = Meteor.user().emails[0].address : name = 'Anonymous';
       var message = $(event.target);
       var messageContent = message.val();
-      console.log(messageContent);
 
-      if (messageContent != '') {
-        Meteor.call('addMessage', {
-          name: name,
-          message: messageContent,
-          time: Date.now(),
-          parent: Session.get('currentChatParent'),
-          fromId: Meteor.userId()
-        });
+      switch(messageContent) {
+        case '/add':
+          Session.set('activeModal', 'newTicket');
 
-        message.val('');
-      }
+        default:
+          Meteor.user() ? name = Meteor.user().emails[0].address : name = 'Anonymous';
+          if (messageContent != '') {
+            Meteor.call('addMessage', {
+              name: name,
+              message: messageContent,
+              time: Date.now(),
+              parent: Session.get('currentChatParent'),
+              fromId: Meteor.userId()
+            });
+            message.val('');
+          };
+      };
     }
   }
 });
