9a46d8fda581091431e9653c564a9052e58ccf5b
andrewliebchen <andrewliebchen@gmail.com>
2014-07-18 23:21:31 -0700

    Add new Meteor hooks to tickets

--- client/templates/tickets/tickets.js
+++ client/templates/tickets/tickets.js
@@ -31,82 +31,49 @@ Template.tickets.rendered = function(){
 };
 
 Template.newTicket.events({
-  'click #add_ticket' : function(event, template) {
+  'click .mrt_add-ticket' : function(event, template) {
     var ticketNumber      = Session.get('ticketCount');
-    var ticketTag         = template.find('#new_ticket_tag');
-    var ticketDescription = template.find('#new_ticket_description');
+    var ticketTag         = template.find('.mrt_new-ticket_tag');
+    var ticketDescription = template.find('.mrt_new-ticket_description');
+    console.log(ticketTag + ticketDescription);
 
     var newTicket = {
       userId:      Meteor.userId(),
       number:      ticketNumber,
       tag:         ticketTag.value,
       description: ticketDescription.value,
-      type:        template.find('#new_ticket_type').value,
-      size:        template.find('#new_ticket_size').value,
-      status:      template.find('#new_ticket_status').value,
+      type:        template.find('.mrt_new-ticket_type').value,
+      size:        template.find('.mrt_new-ticket_size').value,
+      status:      template.find('.mrt_new-ticket_status').value,
       time:        Date.now()
     }
 
-    if(newTicket.title != '') {
-      Meteor.call('addTicket', newTicket, function(err, id){
-        if(err){
-        console.log(err);
-      }
+    if(newTicket.tag != '') {
+      Meteor.call('addTicket', newTicket, function(err, id) {
+        if(err) {
+          console.log(err);
+        }
       });
 
-      ticketTitle.value = '';
+      ticketTag.value = '';
       ticketDescription.value = '';
     }
-  },
-
-  'change #new_ticket_type' : function(event, template) {
-    var type = template.find('#new_ticket_type').value;
-    $('.modal-wrapper')
-      .removeClass('story')
-      .removeClass('defect')
-      .removeClass('task')
-      .addClass(type);
   }
 });
 
 Template.tickets.events({
-
-  'click .ticket-menu-toggle' : function(event) {
-    event.preventDefault();
-    var $ticketParent = $(event.target).closest('.ticket');
-    var height = $ticketParent.outerHeight();
-    $ticketParent.css('height', height);
-    Session.set('ticketMenu', this._id);
-  },
-
-  'click .ticket-delete' : function(event) {
-    event.preventDefault();
-    Meteor.call('removeTicket', this._id);
-  },
-
-  'click .ticket-edit' : function(event) {
-    event.preventDefault();
-    Session.set('editingTicket', this._id);
-  },
-
-  'click #edit_ticket' : function(event, template) {
+  'click .mrt_edit-ticket' : function(event, template) {
     var edits = {
-      tag:    template.find('#edit_ticket_tag').value,
-      type:   template.find('#edit_ticket_type').value,
-      size:   template.find('#edit_ticket_size').value,
-      status: template.find('#edit_ticket_status').value
+      tag:    template.find('.mrt_edit-ticket_tag').value,
+      type:   template.find('.mrt_edit-ticket_type').value,
+      size:   template.find('.mrt_edit-ticket_size').value,
+      status: template.find('.mrt_edit-ticket_status').value
     };
 
     Meteor.call('editTicket', this._id, edits);
 
     Session.set('editingTicket', null);
     isotopeLayout($('.tickets'));
-  },
-
-  'click .close' : function(event) {
-    event.preventDefault();
-    Session.set('editingTicket', null);
-    Session.set('ticketMenu', null);
   }
 });
 
